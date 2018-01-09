224721306bc9a39154941bae0f29e6897a00d59f
Raphaël Reiter <raphaelreiter@Raphaels-iMac.local>
2018-01-09 22:43:09 +0100

    first commit

---
+++ KindleTutorial/BookPagerController.swift
@@ -0,0 +1,18 @@
+
+//
+//  bookPageController.swift
+//  KindleTutorial
+//
+//  Created by Raphaël Reiter on 09/01/2018.
+//  Copyright © 2018 Raphaël Reiter. All rights reserved.
+//
+
+import UIKit
+
+class BookPagerController: UICollectionViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView?.backgroundColor = .white
+    }
+}
---
+++ KindleTutorial/PageCell.swift
@@ -0,0 +1,9 @@
+//
+//  pageCell.swift
+//  KindleTutorial
+//
+//  Created by Raphaël Reiter on 09/01/2018.
+//  Copyright © 2018 Raphaël Reiter. All rights reserved.
+//
+
+import Foundation
