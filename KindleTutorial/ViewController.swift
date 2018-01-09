3d0d792a74e5b5ff6fa2cc85ffa680de47d7f657
Raphaël Reiter <rdso.reiter@gmail.com>
2018-01-09 13:16:50 +0100

    Initial Commit

---
+++ KindleTutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KindleTutorial
+//
+//  Created by Raphaël Reiter on 09/01/2018.
+//  Copyright © 2018 Raphaël Reiter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
